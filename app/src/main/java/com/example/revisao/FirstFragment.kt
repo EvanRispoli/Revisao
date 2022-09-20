15be3540631d14df787014e69ff7e1a71e867acf
Evan Rispoli <evan.rispoli@gmail.com>
2022-09-19 20:31:58 -0400

    IMPLEMENTING NAVIGATION BETWEEN FRAGMENTS

--- app/src/main/java/com/example/revisao/FirstFragment.kt
+++ app/src/main/java/com/example/revisao/FirstFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.revisao.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -17,9 +19,13 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.btnNext.setOnClickListener {
+            view.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
         return view
     }
 
@@ -27,6 +33,4 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
--- app/src/main/java/com/example/revisao/SecondFragment.kt
+++ app/src/main/java/com/example/revisao/SecondFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.revisao.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
     private var _binding: FragmentSecondBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -17,9 +19,13 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.btnBack.setOnClickListener {
+            view.findNavController().navigate(R.id.action_secondFragment_to_firstFragment3)
+        }
         return view
     }
 
@@ -27,6 +33,4 @@ class SecondFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
